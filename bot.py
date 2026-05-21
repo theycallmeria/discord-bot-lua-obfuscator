59a296d0c043f255416aef65848d031447241437
theycallmeria <kanariaazxc@gmail.com>
2026-05-21 14:29:27 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -24,10 +24,10 @@ def keep_alive():
     server.start()
  
 # make sure you add discord bot token in secret environment variables with a key named DISCORD_TOKEN
-token = os.environ['DISCORD_TOKEN']
+token = os.environ['MTQxMTc3NjAwNzYwNTU4ODA3MQ.GCjpYE.QJERlB4HrwyGXo6cy2yuB2IeTgmUAKiJtj9mcM']
 # channel id of the channel you want the bot obfuscate in. 
 # with developer settings enabled right click channel and copy id. (bot works in direct messages as well)
-channel_id = 994709144449851452
+channel_id = 1504367115501568070
 
 bot = commands.Bot(command_prefix="!")
 bot.remove_command("help")
